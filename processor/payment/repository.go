f7744bc23136156a6afb657d0f79116b37aba131
Lefel <felipe.pbgomes@gmail.com>
2020-01-04 22:30:37 -0300

    Made other test of acquirer

--- processor/payment/repository.go
+++ processor/payment/repository.go
@@ -4,5 +4,5 @@ import "processor/payment/models"
 
 type Repository interface {
 	GetCardSensitiveData(token string, errors *models.Error) *models.CardSensitiveData
-	GetAcquirerURL(id int64, errors []error) string
+	GetAcquirerURL(id int64, errors *models.Error) string
 }
--- processor/payment/repository/mysql/mysql_repository_test.go
+++ processor/payment/repository/mysql/mysql_repository_test.go
@@ -45,8 +45,8 @@ func TestGetCardSensitiveData(t *testing.T) {
 	tests["ReturnValue"] = test{
 		Token: "blaoaslkdajeifaepfae.asiudh123123785.3hauk.498a84",
 		ExpectedOutput: &models.CardSensitiveData{
-			Number: 426501269876325,
-			CVV:    123,
+			Number: "426501269876325",
+			CVV:    "123",
 		},
 		ExpectedError: models.Error{
 			Validation: nil,
@@ -58,11 +58,50 @@ func TestGetCardSensitiveData(t *testing.T) {
 		t.Run(title, func(t *testing.T) {
 
 			rows := sqlmock.NewRows([]string{"number", "cvv"}).AddRow(test.ExpectedOutput.Number, test.ExpectedOutput.CVV)
-			mock.ExpectPrepare("SELECT number, cvv FROM cards.cards").ExpectQuery().WithArgs(test.Token).WillReturnRows(rows)
+			mock.ExpectPrepare("SELECT number, cvv FROM processors.cards").ExpectQuery().WithArgs(test.Token).WillReturnRows(rows)
 			oldError := test.ExpectedError
-			ret := repo.GetCardSensitiveData(test.InputName, &test.ExpectedError)
+			ret := repo.GetCardSensitiveData(test.Token, &test.ExpectedError)
 
-			if assert.Equal(t, test.ExpectedError, oldError, "errors we expected: %s, but not nil has return: %s", test.ExpectedError, err) {
+			if assert.Equal(t, oldError, test.ExpectedError, "errors we expected: %s, but not nil has return: %s", test.ExpectedError, err) {
+				assert.Equal(t, test.ExpectedOutput, ret, "We expected return: %#v but got: %#v", test.ExpectedOutput, ret)
+			}
+		})
+	}
+}
+
+func TestGetAcquirerURL(t *testing.T) {
+	repo, err := NewRepository(db)
+
+	if err != nil {
+		assert.FailNowf(t, "error creating repo: %s", err.Error())
+	}
+
+	type test struct {
+		AcquirerID     int64
+		ExpectedOutput string
+		ExpectedError  models.Error
+	}
+
+	var tests = make(map[string]test)
+
+	tests["ReturnValue"] = test{
+		AcquirerID:     1,
+		ExpectedOutput: "http://test.com",
+		ExpectedError: models.Error{
+			Validation: nil,
+			Internal:   nil,
+		},
+	}
+
+	for title, test := range tests {
+		t.Run(title, func(t *testing.T) {
+
+			rows := sqlmock.NewRows([]string{"url"}).AddRow(test.ExpectedOutput)
+			mock.ExpectPrepare("SELECT url FROM processors.acquirers").ExpectQuery().WithArgs(test.AcquirerID).WillReturnRows(rows)
+			oldError := test.ExpectedError
+			ret := repo.GetAcquirerURL(test.AcquirerID, &test.ExpectedError)
+
+			if assert.Equal(t, oldError, test.ExpectedError, "errors we expected: %s, but not nil has return: %s", test.ExpectedError, err) {
 				assert.Equal(t, test.ExpectedOutput, ret, "We expected return: %#v but got: %#v", test.ExpectedOutput, ret)
 			}
 		})
