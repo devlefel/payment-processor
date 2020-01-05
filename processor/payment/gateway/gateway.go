8331a6cfd2134d599d49cbaef1981e08e8775558
Lefel <felipe.pbgomes@gmail.com>
2020-01-05 03:03:44 -0300

    Made gateway implementation and tests

--- processor/payment/gateway/gateway.go
+++ processor/payment/gateway/gateway.go
@@ -1,1 +1,35 @@
 package gateway
+
+import (
+	"processor/core/requester"
+	"processor/payment"
+	"processor/payment/models"
+)
+
+type gateway struct {
+	Repository payment.Repository
+	Requester  requester.Requester
+}
+
+func NewGateway(r payment.Repository, req requester.Requester) payment.Gateway {
+	return &gateway{
+		Repository: r,
+		Requester:  req,
+	}
+}
+
+func (g *gateway) ProcessPayment(card models.CardData, process models.Process, acquirerID int64, errors *models.Error) bool {
+	url := g.Repository.GetAcquirerURL(acquirerID, errors)
+
+	if len(errors.Internal) > 0 || len(errors.Validation) > 0 {
+		return false
+	}
+
+	success := g.Requester.SendBuyRequest(process, card, url, errors)
+
+	if len(errors.Internal) > 0 || len(errors.Validation) > 0 {
+		return false
+	}
+
+	return success
+}
